774e2786ec6e39ad51ad8282a1328f1959016982
stleary-cisco <55999318+stleary-cisco@users.noreply.github.com>
2019-10-02 12:03:38 -0500

    Update access_token.py

--- examples/resources/access_token.py
+++ examples/resources/access_token.py
@@ -1,3 +1,18 @@
+'''
+Copyright (c) 2019 Cisco and/or its affiliates.
+This software is licensed to you under the terms of the Cisco Sample
+Code License, Version 1.1 (the "License"). A copy of the License
+can be found in the LICENSE.TXT file of this software or at
+https://developer.cisco.com/site/license/cisco-sample-code-license/
+All use of the material herein must be in accordance with the terms of
+the License. All rights not expressly granted by the License are
+reserved. Unless required by applicable law or agreed to separately in
+writing, software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+express or implied.
+'''
+
+
 import requests
 
 
