a8f56151b1d5d9ed46b9347e7d62aac9d8838b90
Sean Leary (stleary) <stleary@cisco.com>
2019-10-24 23:01:42 -0500

    rename

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='ftd-api-cookbook',
     version='1.0',
-    packages=['', 'scripts', 'resources'],
+    packages=['', 'ftd_api_scripts', 'ftd_api_resources'],
     package_dir={'': 'examples'},
     url='',
     license='',
