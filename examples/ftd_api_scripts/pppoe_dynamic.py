f540e922d5007771e3e50a20689b9ecfa39be1e9
Sujan Sirivella <ssirivel@cisco.com>
2019-11-05 16:59:23 -0600

    change: pppoe end-to-end example scripts to set an interface to PPPoE (static/dynamic)

--- examples/ftd_api_scripts/pppoe_dynamic.py
+++ examples/ftd_api_scripts/pppoe_dynamic.py
@@ -21,32 +21,20 @@ from ftd_api_resources.interface import get_intf_object
 from ftd_api_resources.pppoe import update_intf_to_pppoe
 
 
-def main():
+def pppoe_dynamic(host, port, user, passwd, intf_hw_name='GigabitEthernet1/1', vpdn_grp_name='my_pppoe_group', pppoe_user='my_pppoe_username', pppoe_password='my_pppoe_password', ppp_auth_type='PAP', pppoe_metric=1, obtain_default_route=True):
     """
     End to end example of code that updates a physical interface to enable PPPoE on it.
     Requires Python v3.0 or greater and the reqeusts library.
     You must update the values for host, port, user, and password to connect to your device.
     """
-    host = 'fdm-ss-5516.cisco.com'
-    port = '443'
-    user = 'admin'
-    passwd = 'Admin123'
-
-    """
-    Example values to enable PPPoE on an interface
-    You need to update these values to suit your needs
-    """
-
-    intf_hw_name = 'GigabitEthernet1/1'
-
     pppoe = {
-        'vpdnGrpName': 'my_pppoe_group',
-        'pppoeUser': 'my_pppoe_username',
-        'pppoePassword': 'my_pppoe_password',
-        'pppAuthType': 'PAP',
-        'pppoeMetric': 1,
-        'setRoute': True,
-        'ipIsDynamic': True,
+        'vpdnGrpName': vpdn_grp_name,
+        'pppoeUser': pppoe_user,
+        'pppoePassword': pppoe_password,
+        'pppAuthType': ppp_auth_type, #available authentication protocols: PAP, CHAP, MSCHAP
+        'pppoeMetric': pppoe_metric,  #integer value with a valid range: 1-255
+        'setRoute': obtain_default_route,  #if True, default route will be obtained from the PPPoE server
+        'ipIsDynamic': True,  #if True, then the ipAddress and Netmask should be set to '' in the ipv4 object of an interface object
         'type': 'pppoverethernet'
     }
 
@@ -70,4 +58,25 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    import sys
+
+    if len(sys.argv) < 5:
+        print("Usage: python ftd_api_scripts/pppoe_dynamic.py host port user passwd [intf_hw_name, vpdn_grp_name, pppoe_user, pppoe_password, ppp_auth_type, pppoe_metric, obtain_default_route]")
+        exit(1)
+
+    host = sys.argv[1]
+    port = sys.argv[2]
+    user = sys.argv[3]
+    passwd = sys.argv[4]
+    # intf_hw_name = sys.argv[5]
+    # vpdn_grp_name = sys.argv[6]
+    # pppoe_user = sys.argv[7]
+    # pppoe_password = sys.argv[8]
+    # ppp_auth_type = sys.argv[9]
+    # pppoe_metric = sys.argv[10]
+    # obtain_default_route = sys.argv[11]
+    # if pppoe_dynamic(host=host, port=port, user=user, passwd=passwd, intf_hw_name=intf_hw_name, vpdn_grp_name=vpdn_grp_name, pppoe_user=pppoe_user, pppoe_password=pppoe_password, ppp_auth_type=ppp_auth_type, pppoe_metric=pppoe_metric, obtain_default_route=obtain_default_route):
+    if pppoe_dynamic(host=host, port=port, user=user, passwd=passwd):
+        exit(0)
+    else:
+        exit(1)
